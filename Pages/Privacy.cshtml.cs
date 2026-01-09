4e2bba5fc7a9a7dbca446b5ff86d0779e9bcc49b
Vit Bojanovsky <vit.bojanovsky@outlook.cz>
2026-01-09 00:42:02 +0000

    SWITCHED TO MVC TEMPLATE

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Runway_Math.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
